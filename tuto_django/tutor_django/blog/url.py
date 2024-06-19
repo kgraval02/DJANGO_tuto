45e921fd69d0ca309c6ee19e74641f039e4d1dea
kgraval02 <kashyap.inexture@gmail.com>
2024-06-19 17:26:03 +0530

    Merge remote-tracking branch 'origin/git_django_tutorial' into git_django_tutorial

--- tuto_django/tutor_django/blog/url.py
+++ tuto_django/tutor_django/blog/url.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .models import PostListView
+from .views import PostListView, PostDetailsView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name = 'blog_home'),
+    path('post/<int:pk>/', PostDetailsView.as_view(), name = 'post_detail'),
     path('about/', views.about, name='blog_about'),
 ]
 
