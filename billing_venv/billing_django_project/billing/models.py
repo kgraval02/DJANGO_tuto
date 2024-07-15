b6bd90f43203a555253bdf12a7d62908ffc2b1f6
kgraval02 <kashyap.inexture@gmail.com>
2024-07-15 19:00:50 +0530

    this is updated project commited on 05-07

--- billing_venv/billing_django_project/billing/models.py
+++ billing_venv/billing_django_project/billing/models.py
@@ -37,3 +37,20 @@ class InvoiceItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=30)
+    artist = models.CharField(max_length=30)
+    genre = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=100)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
