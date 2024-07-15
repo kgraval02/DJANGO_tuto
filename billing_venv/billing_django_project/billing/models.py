d9d0b53347c2151187cbb4a73eee6ffae389fecf
kgraval02 <kashyap.inexture@gmail.com>
2024-07-15 15:39:27 +0530

    this is updated project commited on 05-07

--- billing_venv/billing_django_project/billing/models.py
+++ billing_venv/billing_django_project/billing/models.py
@@ -1,11 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
 from product_grocery.models import Product, Category
+from django.db import models
+
 
 class Feedback(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
     reviewed = models.BooleanField(default=False, verbose_name='Reviewed')
 
     def __str__(self):
@@ -17,16 +18,19 @@ class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     added_at = models.DateTimeField(auto_now_add=True)
 
+
 class Invoice(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 class InvoiceItem(models.Model):
     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
--- billing_venv/billing_django_project/billing/templatetags/custom_tags.py
+++ billing_venv/billing_django_project/billing/templatetags/custom_tags.py
@@ -1,5 +1,6 @@
 from django.templatetags.l10n import register
 from product_grocery.models import Product
+from django import template
 
 
 @register.filter()
@@ -7,17 +8,28 @@ def productimage(pid):
     data = Product.objects.get(id=pid)
     return data.image.url
 
+
 @register.filter()
 def productname(pid):
     data = Product.objects.get(id=pid)
     return data.name
+
 
 @register.filter()
 def productprice(pid):
     data = Product.objects.get(id=pid)
     return data.price
 
+
 @register.simple_tag()
 def producttotalprice(pid, qty):
     data = Product.objects.get(id=pid)
     return int(qty) * int(data.price)
+
+
+register = template.Library()
+
+
+@register.filter
+def multiply(value, arg):
+    return value * arg
